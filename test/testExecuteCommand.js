b7e38c9db6a1931c96f51269366384666eb6406b
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2019-12-24 18:50:26 +0530

    added more test cases

--- test/testExecuteCommand.js
+++ test/testExecuteCommand.js
@@ -17,4 +17,38 @@ describe("executeCut", () => {
     const expected = { msg: ["2", "2"] };
     assert.deepStrictEqual(executeCut(cutInfo, fsTools), expected);
   });
+  it("should give the corresponding error message  when the field is not given", () => {
+    const read = function(path, encoding) {
+      assert.strictEqual("./test/testFile", path);
+      assert.strictEqual("utf8", encoding);
+      return "1,2\n1,2,3";
+    };
+
+    const fsTools = {
+      read: read,
+      encoding: "utf8"
+    };
+
+    const cutInfo = ["node", "cut.js", "2", "-d", ",", "./test/testFile"];
+    const expected = { error: `usage: cut -f list [-d delim] [file]` };
+    assert.deepStrictEqual(executeCut(cutInfo, fsTools), expected);
+  });
+  it("should give the corresponding error message  when the file is missing", () => {
+    const read = function(path, encoding) {
+      assert.strictEqual("./test/testFie", path);
+      assert.strictEqual("utf8", encoding);
+      return "1,2\n1,2,3";
+    };
+
+    const fsTools = {
+      read: read,
+      encoding: "utf8"
+    };
+
+    const cutInfo = ["node", "cut.js", "-f", "2", "-d", ",", "./test/testFile"];
+    const expected = {
+      error: `cut: ./test/testFile: No such file or directory`
+    };
+    assert.deepStrictEqual(executeCut(cutInfo, fsTools), expected);
+  });
 });
