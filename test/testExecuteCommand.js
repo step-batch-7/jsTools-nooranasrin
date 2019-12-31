6673072120120bc6930f28c0b879f8494bfb16ed
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2019-12-31 11:10:20 +0530

    updated testing of executeCut function

--- test/testExecuteCommand.js
+++ test/testExecuteCommand.js
@@ -1,9 +1,9 @@
 const assert = require('chai').assert;
-const EventEmitter = require('events');
 const sinon = require('sinon');
 const { executeCut } = require('../src/executeCommand');
 describe('executeCut', () => {
   it('should give corresponding error message when -f is missing', () => {
+    afterEach(() => sinon.restore());
     const onComplete = sinon.stub();
     const inputStreams = { createReadStream: '', stdin: '' };
     const actual = executeCut([], inputStreams, onComplete);
@@ -13,53 +13,83 @@ describe('executeCut', () => {
     );
   });
 
-  it('should give error message when the file is not existing', () => {
-    const userArgs = ['-f', '1', '-d', ',', 'fileName'];
-    const onComplete = sinon.stub();
-    const eventEmitter = new EventEmitter();
-    eventEmitter.setEncoding = sinon.spy();
-    const createReadStream = sinon.fake.returns(eventEmitter);
-    const inputStreams = { createReadStream, stdin: '' };
-    executeCut(userArgs, inputStreams, onComplete);
-    assert(createReadStream.calledOnceWithExactly('fileName'));
-    eventEmitter.emit('error', { code: 'ENOENT' });
-    assert(onComplete.calledWithExactly('cut: No such file or directory', ''));
+  it('should give error message when the file is not existing', done => {
+    afterEach(() => sinon.restore());
+    const onComplete = function(error, content) {
+      assert.strictEqual(error, 'cut: No such file or directory');
+      assert.strictEqual(content, '');
+      done();
+    };
+    const readStream = { setEncoding: sinon.fake(), on: sinon.fake() };
+    const createReadStream = sinon.fake.returns(readStream);
+    executeCut(
+      ['-f', '1', '-d', ',', 'num.txt'],
+      { createReadStream, stdin: '' },
+      onComplete
+    );
+    assert(readStream.setEncoding.calledWith('utf8'));
+    assert.strictEqual(readStream.on.firstCall.args[0], 'data');
+    assert.strictEqual(readStream.on.secondCall.args[0], 'error');
+    assert.isTrue(readStream.on.calledTwice);
+    readStream.on.secondCall.args[1]({ code: 'ENOENT' });
   });
 
-  it('should give ENOENT error when the error code is not expected', () => {
-    const userArgs = ['-f', '1', '-d', ',', 'fileName'];
-    const onComplete = sinon.stub();
-    const eventEmitter = new EventEmitter();
-    eventEmitter.setEncoding = sinon.spy();
-    const createReadStream = sinon.fake.returns(eventEmitter);
-    const inputStreams = { createReadStream, stdin: '' };
-    executeCut(userArgs, inputStreams, onComplete);
-    assert(createReadStream.calledOnceWithExactly('fileName'));
-    eventEmitter.emit('error', { code: 'ENOEN' });
-    assert(onComplete.calledWithExactly('cut: No such file or directory', ''));
+  it('should give ENOENT error when the error code is not expected', done => {
+    afterEach(() => sinon.restore());
+    const onComplete = function(error, content) {
+      assert.strictEqual(error, 'cut: No such file or directory');
+      assert.strictEqual(content, '');
+      done();
+    };
+    const readStream = { setEncoding: sinon.fake(), on: sinon.fake() };
+    const createReadStream = sinon.fake.returns(readStream);
+    executeCut(
+      ['-f', '1', '-d', ',', 'num.txt'],
+      { createReadStream, stdin: '' },
+      onComplete
+    );
+    assert(readStream.setEncoding.calledWith('utf8'));
+    assert.strictEqual(readStream.on.firstCall.args[0], 'data');
+    assert.strictEqual(readStream.on.secondCall.args[0], 'error');
+    assert.isTrue(readStream.on.calledTwice);
+    readStream.on.secondCall.args[1]({ code: 'ENOEN' });
   });
 
-  it('should give expected fields when the file is existing', () => {
-    const userArgs = ['-f', '1', '-d', ',', 'fileName'];
-    const onComplete = sinon.stub();
-    const eventEmitter = new EventEmitter();
-    eventEmitter.setEncoding = sinon.spy();
-    const createReadStream = sinon.fake.returns(eventEmitter);
-    const inputStreams = { createReadStream, stdin: '' };
-    executeCut(userArgs, inputStreams, onComplete);
-    assert(createReadStream.calledOnceWithExactly('fileName'));
-    eventEmitter.emit('data', '1,2,3,4\n2,3');
-    assert(onComplete.calledOnceWithExactly('', '1\n2'));
+  it('should give expected fields when the file is existing', done => {
+    afterEach(() => sinon.restore());
+    const onComplete = function(error, content) {
+      assert.strictEqual(error, '');
+      assert.strictEqual(content, '1');
+      done();
+    };
+    const readStream = { setEncoding: sinon.fake(), on: sinon.fake() };
+    const createReadStream = sinon.fake.returns(readStream);
+    executeCut(
+      ['-f', '1', '-d', ',', 'num.txt'],
+      { createReadStream, stdin: '' },
+      onComplete
+    );
+    assert(readStream.setEncoding.calledWith('utf8'));
+    assert.strictEqual(readStream.on.firstCall.args[0], 'data');
+    assert.strictEqual(readStream.on.secondCall.args[0], 'error');
+    assert.isTrue(readStream.on.calledTwice);
+    readStream.on.firstCall.args[1]('1,2,3');
   });
 
-  it('should give expected fields in case of stdin', () => {
-    const userArgs = ['-f', '1', '-d', ','];
-    const onComplete = sinon.stub();
-    const stdin = new EventEmitter();
-    stdin.setEncoding = sinon.spy();
+  it('should give expected fields in case of stdin', done => {
+    afterEach(() => sinon.restore());
+    const onComplete = function(error, content) {
+      assert.strictEqual(error, '');
+      assert.strictEqual(content, '1');
+      done();
+    };
+    const stdin = { setEncoding: sinon.fake(), on: sinon.fake() };
     const inputStreams = { createReadStream: '', stdin };
-    executeCut(userArgs, inputStreams, onComplete);
-    stdin.emit('data', '1,2,3,4\n2,3');
-    assert(onComplete.calledOnceWithExactly('', '1\n2'));
+    executeCut(['-f', '1', '-d', ','], inputStreams, onComplete);
+    assert(stdin.setEncoding.calledWith('utf8'));
+    assert.strictEqual(stdin.on.firstCall.args[0], 'data');
+    assert.strictEqual(stdin.on.secondCall.args[0], 'error');
+    assert.isTrue(stdin.on.calledTwice);
+    stdin.on.firstCall.args[1]('1,2,3');
   });
 });
